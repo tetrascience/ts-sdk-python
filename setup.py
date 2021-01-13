f72b63fb3bf7fcfbfa73f3255cdd419db04c6962
Yury Andryianets <yandryianets@tetrascience.com>
2021-01-13 21:45:58 +0300

    DI-3148 setup deps versions range fix

--- setup.py
+++ setup.py
@@ -14,25 +14,25 @@ VERSION = "1.0.0"
 
 # Should we import info from requirements.txt ???
 REQUIRES = [
-    "attrs>=20.2.0",
-    "boto3>=1.16.3",
-    "botocore>=1.19.3",
-    "certifi>=2020.6.20",
-    "chardet>=3.0.4",
-    "idna>=2.10",
-    "importlib-metadata>=2.0.0",
-    "jmespath>=0.10.0",
-    "jsonschema>=3.2.0",
-    "pyrsistent>=0.17.3",
-    "python-dateutil>=2.8.1",
-    "query-string>=2020.7.1",
-    "requests>=2.24.0",
-    "s3transfer>=0.3.3",
-    "six>=1.15.0",
-    "smart-open[s3]>=3.0.0",
-    "typing-extensions>=3.7.4.3",
-    "urllib3>=1.25.11",
-    "zipp>=3.3.1"
+    "attrs>=20.2.0,<21",
+    "boto3>=1.16.3,<2",
+    "botocore>=1.19.3,<2",
+    "certifi>=2020.6.20,<2021",
+    "chardet>=3.0.4,<4",
+    "idna>=2.10,<3",
+    "importlib-metadata>=2.0.0,<3",
+    "jmespath>=0.10.0,<1",
+    "jsonschema>=3.2.0,<4",
+    "pyrsistent>=0.17.3,<1",
+    "python-dateutil>=2.8.1,<3",
+    "query-string>=2020.7.1,<2021",
+    "requests>=2.24.0,<3",
+    "s3transfer>=0.3.3,<1",
+    "six>=1.15.0,<2",
+    "smart-open[s3]>=3.0.0,<4",
+    "typing-extensions>=3.7.4,<4",
+    "urllib3>=1.25.11,<2",
+    "zipp>=3.3.1,<4"
     ]
 
 setup(
