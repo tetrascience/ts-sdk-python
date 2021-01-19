57233d86f157d198014d61e464263e48468c67ca
Yury Andryianets <yandryianets@tetrascience.com>
2021-01-20 01:12:03 +0300

    DI-3189 fix setup.py version

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ts-sdk",
-    version="1.0.5-rc", # substitute by tag during travis build!
+    version="1.0.6-rc",
     description="Tetrascience Python SDK",
     long_description=long_description,
     long_description_content_type="text/markdown",
