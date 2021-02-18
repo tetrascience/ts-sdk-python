215889088b5e9dcff2dd22e41bd45624c2f5b18e
Yury Andryianets <yandryianets@tetrascience.com>
2021-02-18 12:37:50 +0300

    DI-3465 tests fix

--- __test__/test_taskdev.py
+++ __test__/test_taskdev.py
@@ -3,9 +3,9 @@ from ts_sdk.taskdev import Context, load_test_file, load_pipeline_config, check_
 def test_taskdev_should_load_text_file():
     c = Context({}) 
     f = load_test_file(c, __file__, '/datalake/path', file_category='RAW')
-    assert f == {'type': 's3', 'bucket': 'fake-unittest-bucket', 'fileKey': '/datalake/path'}
+    assert f == {'type': 's3file', 'bucket': 'fake-unittest-bucket', 'fileKey': '/datalake/path'}
 
 def test_taskdev_should_load_bin_file():
     c = Context({}) 
     f = load_test_file(c, '/bin/ls', '/datalake/path', file_category='RAW', mode='rb')
-    assert f == {'type': 's3', 'bucket': 'fake-unittest-bucket', 'fileKey': '/datalake/path'}
+    assert f == {'type': 's3file', 'bucket': 'fake-unittest-bucket', 'fileKey': '/datalake/path'}
