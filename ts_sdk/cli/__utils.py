0a89f0c7fbd0543bf8a98a04128268b06c2272d0
Yury Andryianets <yandryianets@tetrascience.com>
2021-03-05 19:45:24 +0300

    DI-3387 allow dot files during put

--- ts_sdk/cli/__utils.py
+++ ts_sdk/cli/__utils.py
@@ -52,7 +52,6 @@ def sizeof_fmt(num, suffix='B'):
 def zipdir(path: str, z: zipfile.ZipFile):
     for root, dirs, files in os.walk(path):
         for file in files:
-            if not os.path.basename(file).startswith('.'):
-                local_path = os.path.join(root, file)
-                zip_path = os.path.relpath(os.path.join(root, file), os.path.join(path))
-                z.write(local_path, zip_path)
+            local_path = os.path.join(root, file)
+            zip_path = os.path.relpath(os.path.join(root, file), os.path.join(path))
+            z.write(local_path, zip_path)
