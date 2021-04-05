ef0e6e3198bb1a2ac31dc825c2286bebdab94d55
Yury Andryianets <70173734+yandryianets@users.noreply.github.com>
2021-04-05 22:57:00 +0300

    DI-3721 ts-sdk: multiprocessing fix (#39)
    * DI-3721 ts-sdk: multiprocessing fix
    
    * DI-3721 fix
    
    * DI-3721 kill() instead terminate()
    
    * DI-3721 fix
    
    * DI-3721 PR fixes

--- __test__/test_run_reuse.py
+++ __test__/test_run_reuse.py
@@ -1,6 +1,6 @@
 import sys
 from unittest.mock import patch
-from ts_sdk.task.run_reuse_loop import task_worker, last_run
+from ts_sdk.task.run_reuse_loop import task_process_fn
 
 task = {
     'context': {},
@@ -12,27 +12,25 @@ def __sys_exit_side_effect(*args, **kwargs):
     sys.exit('Test exit')
 
 @patch('ts_sdk.task.run_reuse_loop.run')
-def test_task_worker_should_return_result(patched_run):
-    last_run['result'] = None
-    last_run['error'] = None
+def test_task_process_fn_should_return_result(patched_run):
+    shared_dict = {'result': None, 'error': None}
     patched_run.return_value = 'ok'
-    task_worker(task)
-    assert last_run == {'error': None, 'result': 'ok'}
+    task_process_fn(task, shared_dict)
+    assert shared_dict['error'] == None
+    assert shared_dict['result'] == 'ok'
 
 @patch('ts_sdk.task.run_reuse_loop.run')
-def test_task_worker_should_return_error(patched_run):
-    last_run['result'] = None
-    last_run['error'] = None
+def test_task_process_fn_should_return_error(patched_run):
+    shared_dict = {'result': None, 'error': None}
     patched_run.side_effect = Exception('Boom!')
-    task_worker(task)
-    assert last_run['result'] == None
-    assert 'Traceback' in last_run['error']
+    task_process_fn(task, shared_dict)
+    assert shared_dict['result'] == None
+    assert 'Traceback' in shared_dict['error']
 
 @patch('ts_sdk.task.run_reuse_loop.run')
-def test_task_worker_should_handle_sysexit(patched_run):
-    last_run['result'] = None
-    last_run['error'] = None
+def test_task_process_fn_should_handle_sysexit(patched_run):
+    shared_dict = {'result': None, 'error': None}
     patched_run.side_effect = __sys_exit_side_effect
-    task_worker(task)
-    assert last_run['result'] == None
-    assert 'Traceback' in last_run['error']
+    task_process_fn(task, shared_dict)
+    assert shared_dict['result'] == None
+    assert 'Traceback' in shared_dict['error']
