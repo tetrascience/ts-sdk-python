8e3020b1a9b0562dbefb8885517eab1f3011b335
Yury Andryianets <70173734+yandryianets@users.noreply.github.com>
2021-04-06 18:18:03 +0300

    Check file existence in db before labels operations + x-pipeline-id header (#38)
    * Check file existence in db before labels operations
    * x-pipeline-id header

--- ts_sdk/task/__util_fileinfo.py
+++ ts_sdk/task/__util_fileinfo.py
@@ -1,18 +1,35 @@
 import json
 import requests
+from time import sleep
 
 class Fileinfo:
 
     def __init__(self, endpoint):
         self.endpoint = endpoint
 
-    def add_labels(self, context_data, file_id, labels):
+    def ensure_file_exists_in_db(self, file_id, org_slug, check_delay = 2, attempts_max = 10):
+        url = f'{self.endpoint}/internal/{org_slug}/file-exists/{file_id}'
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        attempt = 0
+        while attempt < attempts_max:
+            sleep(check_delay)
+            response = requests.request('GET', url, headers=headers, verify=False)
+            if response.status_code == 200:
+                return True
+            attempt = attempt + 1
+        raise Exception('File existence check failed') 
 
+    def add_labels(self, context_data, file_id, labels):
         org_slug = context_data.get('orgSlug')
+        self.ensure_file_exists_in_db(file_id, org_slug)
+
         url = f'{self.endpoint}/internal/{org_slug}/files/{file_id}/labels'
 
         headers = {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'x-pipeline-id': context_data.get('pipelineId')
         }
         response = requests.request('POST', url, headers=headers, data=json.dumps(labels), verify=False)
 
@@ -25,6 +42,8 @@ class Fileinfo:
 
     def get_labels(self, context_data, file_id):
         org_slug = context_data.get('orgSlug')
+        self.ensure_file_exists_in_db(file_id, org_slug)
+
         url = f'{self.endpoint}/internal/{org_slug}/files/{file_id}/labels'
         headers = {
             'Content-Type': 'application/json'
@@ -40,12 +59,14 @@ class Fileinfo:
 
     def delete_labels(self, context_data, file_id, label_ids):
         org_slug = context_data.get('orgSlug')
+        self.ensure_file_exists_in_db(file_id, org_slug)
 
         suffix = '&'.join(map(lambda id: 'id=' + str(id), label_ids))
         url = f'{self.endpoint}/internal/{org_slug}/files/{file_id}/labels?{suffix}'
 
         headers = {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'x-pipeline-id': context_data.get('pipelineId')
         }
         response = requests.request('DELETE', url, headers=headers, verify=False)
 
